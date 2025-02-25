8d1d897bfce7eba0d3798cd3a6aa91b40d418272
Jucazuve1965 <zunigajuancarlos@yahoo.com>
2025-02-25 08:41:55 -0500

    20250225 tarea calculadora

---
+++ app.js
@@ -0,0 +1,60 @@
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function appendOperator(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculateResult();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function clearDisplay() {
+    currentInput = '';
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function calculateResult() {
+    if (previousInput === '' || currentInput === '') return;
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            if (current === 0) {
+                result = 'Error';
+            } else {
+                result = prev / current;
+            }
+            break;
+        default:
+            return;
+    }
+    currentInput = result.toString();
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = currentInput;
+}
